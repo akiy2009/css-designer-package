391a92c093ab5a4c7910c1807874d4d1c8993d57
Aki <y.akitaka2009@icloud.com>
2025-12-09 11:25:21 +0900

    index.js を作成

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import {
+  autoContrast,
+  adjustHue,
+  adjustLightness,
+  adjustSaturation,
+  contrastRatio,
+  luminance
+} from "./color-utils.js";
+
+export default {
+  autoContrast,
+  adjustHue,
+  adjustLightness,
+  adjustSaturation,
+  contrastRatio,
+  luminance
+};
